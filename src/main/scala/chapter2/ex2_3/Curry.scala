8f2a25265fa3a8c1fcf3cac0c228f0b284868497
Yusuke Wada <wada.yusuke@classmethod.jp>
2017-11-09 18:41:38 +0900

    [fix] 読書会を受けて修正・追加。

--- src/main/scala/chapter2/ex2_3/Curry.scala
+++ src/main/scala/chapter2/ex2_3/Curry.scala
@@ -2,6 +2,11 @@ package chapter2.ex2_3
 
 object Curry {
 
-  def curry[A, B, C](f: (A, B) => C): A => (B => C) = { a => f(a, _) }
+  def partial1[A, B, C](a: A, f: (A, B) => C): B => C = (b: B) => f(a, b)
+
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) = (a: A) => f(a, _)
+
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) = (a: A) => (b: B) => f(a, b)
+
 
 }
--- src/main/scala/chapter2/ex2_4/Uncurry.scala
+++ src/main/scala/chapter2/ex2_4/Uncurry.scala
@@ -2,6 +2,7 @@ package chapter2.ex2_4
 
 object Uncurry {
 
+  // braceいらない
   def uncurry[A, B, C](f: A => B => C): (A, B) => C = { (a, b) => f(a)(b) }
 
 }
