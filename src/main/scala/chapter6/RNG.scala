9d39cfda7cfe88640c18cc31d926903e7d940e49
Yusuke Wada <wada.yusuke@classmethod.jp>
2018-03-14 09:34:48 +0900

    [add] - ex6.4

--- src/main/scala/chapter6/RNG.scala
+++ src/main/scala/chapter6/RNG.scala
@@ -24,4 +24,44 @@ object EXRNG {
     (i & Int.MaxValue, r) // 最上位ビット反転 = 符号反転
   }
 
+  // EX 6.2
+  def double(rng: RNG): (Double, RNG) = {
+    val (i, r) = nonNegativeInt(rng)
+    (i.toDouble / Int.MaxValue, r)
+  }
+
+
+  // EX6.3
+  // RNGを伝搬させるのか、同じRNGを使いまわすのか迷った
+  // 後続の関数と差を出すには伝搬させるのが妥当だろうという判断
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i, ir) = nonNegativeInt(rng)
+    val (d, dr) = double(ir)
+    ((i, d), dr)
+  }
+
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val (d, dr) = double(rng)
+    val (i, ir) = nonNegativeInt(dr)
+
+    ((d, i), ir)
+  }
+
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1, r1) = double(rng)
+    val (d2, r2) = double(r1)
+    val (d3, r3) = double(r2)
+
+    ((d1, d2, d3), r3)
+  }
+
+  // EX6.4
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) =
+    List.fill(count)(0).foldLeft((List.empty[Int], rng)) {
+      case ((list, tr), _) =>
+        val (ni, nr) = nonNegativeInt(tr)
+        (list :+ ni, nr)
+    }
+
+
 }
--- src/test/scala/chapter6/Chapter6Test.scala
+++ src/test/scala/chapter6/Chapter6Test.scala
@@ -20,9 +20,37 @@ class Chapter6Test extends FlatSpec with Matchers {
     val (nn3, nrng3) = EXRNG.nonNegativeInt(nrng2)
 
 
+    (n, n2, n3) shouldEqual(769497, 1988230381, -1277571485)
+    (nn, nn2, nn3) shouldEqual(769497, 1988230381, 869912163)
 
-    (n, n2, n3) shouldEqual (769497,1988230381,-1277571485)
-    (nn, nn2, nn3) shouldEqual (769497,1988230381,869912163)
+  }
+
+  "ex62" should "output" in {
+
+    val sut = new SimpleRNG(2)
+
+    val (n, rng) = sut.nextInt
+    val (n2, rng2) = rng.nextInt
+    val (n3, rng3) = rng2.nextInt
+
+    val (nn, nrng) = EXRNG.double(sut)
+    val (nn2, nrng2) = EXRNG.double(nrng)
+    val (nn3, nrng3) = EXRNG.double(nrng2)
+
+
+    (n, n2, n3) shouldEqual(769497, 1988230381, -1277571485)
+    List(nn, nn2, nn3).map(_ * 10000).map(_.toInt) shouldEqual List(3, 9258, 4050)
+
+  }
+
+  "ex64" should "output" in {
+
+    val sut = new SimpleRNG(2)
+
+    val (ns, nrng) = EXRNG.ints(5)(sut)
+
+    ns shouldEqual List(769497, 1988230381, 869912163, 319775261, 962104480)
+
 
   }
 
