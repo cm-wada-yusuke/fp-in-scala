9b42d033fea938341a055558f0e1378ba15e7c52
Yusuke Wada <wada.yusuke@classmethod.jp>
2018-04-12 14:03:58 +0900

    理解を追記

--- src/main/scala/chapter6/VM.scala
+++ src/main/scala/chapter6/VM.scala
@@ -64,9 +64,18 @@ object VM {
   }
 
 
-  // あんまりカタルシスがない…絶対他にやり方ある…
+  // 1. 入力がリストで、出力がStateなので、これを満たせそうなメソッドは sequence だけ。これを使う方向。
+  // 2. sequence は出力として値リストが得られるが、これは使わなくて良い
+  // 3. 出力の型を満たそうと思ったら、getで最後の状態を取得、そこから計算した値がそのまま欲しいスナックとコインの値になる。ここは問題ない。
+  // 4. あとはsequenceの中身。sequenceの引数型に合わせる必要があるため、引数は List[State[Machine,なんでも良い]] とする必要がある。
+  // 4-1. ということで inputs.map は確定。 Input => State[Machine,Unit] としたい。
+  // 4-2. 何もないところから State を作る方法… get か set か modify。
+  // 4-3. Input => set(doSomething(i)) でいける？
+  // 4-4. できない。 Input を受け取って何をするかというと、「Machine をつくる」 ではなくて 「Machine を変化させる」 だから。
+  // 4-5. つまり doSomething は Input => (Machine => Machine) というシグネチャになる。
+  // 4-6. (Machine => Machine) から State は作れるか？ 作れる。 def modify[S](f: S => S): State[S, Unit] ここで勝利を確信する。
   def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = for {
-    _ <- sequence(inputs.map((i: Input) => modify[Machine](operation(i)))) // パズルしてたらたまたまできた…ごめんなさいよくわかってない
+    _ <- sequence(inputs.map((i: Input) => modify[Machine](operation(i))))
     s <- get
   } yield (s.coins, s.candies)
 }
